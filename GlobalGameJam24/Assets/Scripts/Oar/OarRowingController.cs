0a5a20af39ffa43e4276a896747b3b9d2061af42
Jason Xu <lemonade3532@outlook.com>
2024-01-25 21:23:45 -0800

    added oar rowing controller

--- GlobalGameJam24/Assets/Scripts/Oar/OarRowingController.cs
+++ GlobalGameJam24/Assets/Scripts/Oar/OarRowingController.cs
@@ -6,14 +6,23 @@ public class OarRowingController : MonoBehaviour
 {
 	public float LerpSpeed = 0.25f;
 	public float WaterHeight = 0.5f;
+	public float ForceMultiplier = 100f;
+	
+
 	[Header("Boat")]
 	public Rigidbody2D BoatRigidbody;
-	public Vector3 BoatLeftPoint;
-	public Vector3 BoatRightPoint;
+	public Vector3 BoatLeftPointLocalOffset;
+	public Vector3 BoatRightPointLocalOffset;
+	public Vector3 PushForceLeft2Right;
+	public Vector3 PushForceRight2Left;
 
 	[SerializeField]
 	protected float _underwaterVelocity;
 	protected Vector3 _lastPosition;
+	protected Vector3 _boatLeftPoint;
+	protected Vector3 _boatRightPoint;
+	protected Vector3 _pushForce;
+
 	
 	public bool IsUnderWater => transform.position.y < WaterHeight;
 
@@ -30,6 +39,11 @@ public class OarRowingController : MonoBehaviour
 	{
 		UpdateVelocity();
 
+		_boatLeftPoint = BoatRigidbody.transform.TransformPoint(BoatLeftPointLocalOffset);
+		_boatRightPoint = BoatRigidbody.transform.TransformPoint(BoatRightPointLocalOffset);
+
+		PushBoat();
+
 		_lastPosition = transform.position;
 	}
 
@@ -45,10 +59,45 @@ public class OarRowingController : MonoBehaviour
 		_underwaterVelocity = Mathf.MoveTowards(_underwaterVelocity, immediateVelocity, LerpSpeed * Time.fixedDeltaTime);
 	}
 
+	private void PushBoat()
+	{
+		if (_underwaterVelocity == 0)
+			return;
+
+		// Push force is applied to the opposite side of the boat
+		if (_underwaterVelocity > 0)
+			_pushForce = PushForceRight2Left;
+		else
+			_pushForce = PushForceLeft2Right;
+
+		// Push force is multiplied by the velocity of the oar
+		_pushForce *= Mathf.Abs(_underwaterVelocity) * ForceMultiplier;
+
+		// Push force is applied
+		BoatRigidbody.AddForceAtPosition(_pushForce, _boatLeftPoint);
+	}
+
 
 	private void OnDrawGizmosSelected()
 	{
 		Gizmos.color = IsUnderWater ? Color.blue : Color.green;
 		Gizmos.DrawLine(new Vector3(-10, WaterHeight, 0), new Vector3(10, WaterHeight, 0));
+
+		Gizmos.color = Color.red;
+		Gizmos.DrawLine(transform.position, transform.position + _pushForce);
+
+
+		if (!Application.isPlaying && BoatRigidbody != null)
+		{
+			_boatLeftPoint = BoatRigidbody.transform.TransformPoint(BoatLeftPointLocalOffset);
+			_boatRightPoint = BoatRigidbody.transform.TransformPoint(BoatRightPointLocalOffset);
+
+			Gizmos.color = Color.red;
+			Gizmos.DrawSphere(_boatLeftPoint,  .05f);
+			Gizmos.DrawSphere(_boatRightPoint, .05f);
+
+			Gizmos.DrawLine(_boatLeftPoint, _boatLeftPoint + PushForceLeft2Right);
+			Gizmos.DrawLine(_boatRightPoint, _boatRightPoint + PushForceRight2Left);
+		}
 	} 
 }
