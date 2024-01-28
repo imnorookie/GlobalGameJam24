4e08720c17ab71e33c35d6e87e2d7f0b6ea222e4
imnorookie <liquid.snake2k20@gmail.com>
2024-01-27 18:55:53 -0800

    Add BGM and Ambience. BGM only plays when in-game, stops whenever you are in the main menu. No U.I. elements for the menu yet, if there are no players currently spawned you are either in the GameOver screen or Main Menu. Press Enter to start the game - and thus the background music. On GameOver, press Escape to head back to the Main Menu (stopping the music but not the ambience) or press Enter to start the game and the music again.

--- GlobalGameJam24/Assets/Scripts/GameManager/GameManager.cs
+++ GlobalGameJam24/Assets/Scripts/GameManager/GameManager.cs
@@ -23,18 +23,40 @@ public class GameManager : MonoBehaviour
     private int[] pRdsWon = new int[2];
     private GameObject[] players = new GameObject[2];
 
+    private bool playBGM = false;
+
+    private AudioSource BGM;
+
+    enum GameStates
+    {
+        Playing,
+        GameOver,
+        MainMenu,
+        Paused
+    }
+
+    private GameStates gameState;
+
     private void Start() {
-        spawnPlayers();
+        // Start Background Ambience
+        SoundManager._instance.PlayBGAmbience();
+        BGM = SoundManager._instance.PlayBGM();
+        BGM.Stop();
+
+        // Start main menu
+        gameState = GameStates.MainMenu;
     }
 
     public void RoundEnd(int player) {
-        if (++pRdsWon[player] >= m_rdsToWin) {
-            pRdsWon = new int[2];
-            Debug.Log("Player " + (player + 1) + " won!");
-        }
-
         foreach (Object p in players) {
             Destroy(p);
+        }
+
+        if (++pRdsWon[player] >= m_rdsToWin) { // WIN CON.
+            pRdsWon = new int[2];
+            Debug.Log("Player " + (player + 1) + " won!");
+            gameState = GameStates.GameOver;
+            return;
         }
 
         playRdEndAnimation();
@@ -50,9 +72,49 @@ public class GameManager : MonoBehaviour
         players[1] = Instantiate(m_p2Prefab, m_p2StartPos, new Quaternion());
     }
 
+    private void startRound() {
+        gameState = GameStates.Playing;
+        BGM.Play();
+        spawnPlayers();
+    }
+
+    private void handleMainMenuInput() {
+        // TODO STUB
+        if (Input.GetKey(KeyCode.Return)) {
+            startRound();
+        }
+
+    }
+
+    private void handlePauseMenuInput() {
+
+    }
+
+    private void handleGameOverInput() {
+        // TODO STUB
+        if (Input.GetKey(KeyCode.Return)) {
+            startRound();
+        } else if (Input.GetKey(KeyCode.Escape)) {
+            gameState = GameStates.MainMenu;
+            BGM.Stop();
+        }
+    }
+
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+        switch (gameState) {
+            case GameStates.MainMenu:
+                handleMainMenuInput();
+                break;
+            case GameStates.Paused:
+                handlePauseMenuInput();
+                break;
+            case GameStates.Playing:
+                break;
+            case GameStates.GameOver:
+                handleGameOverInput();
+                break;
+        }
         
     }
 }
--- GlobalGameJam24/Assets/Scripts/GameManager/SoundManager.cs
+++ GlobalGameJam24/Assets/Scripts/GameManager/SoundManager.cs
@@ -13,76 +13,154 @@ public class SoundManager : MonoBehaviour
     private AudioClip m_boatCollisionAudio;
 
     [SerializeField]
+    [Range(0.0f, 1.0f)]
+    private float m_boatCollisionVol = 1.0f;
+
+    [SerializeField]
     private AudioClip m_oarCollisionAudio;
 
     [SerializeField]
+    [Range(0.0f, 1.0f)]
+    private float m_oarCollisionVol = 1.0f;
+
+
+    [SerializeField]
     private AudioClip m_oarOnPlayerCollisionAudio;
+
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
+    private float m_oarOnPlayerCollisionVol = 1.0f;
     
     [SerializeField]
     private AudioClip[] m_oarOnPlayerHurtAudio;
 
     [SerializeField]
+    [Range(0.0f, 1.0f)]
+    private float m_oarOnPlayerHurtVol = 1.0f;
+
+
+    [SerializeField]
     private AudioClip m_fishOnPlayerCollisionAudio;
+
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
+    private float m_fishOnPlayerCollisionVol = 1.0f;
+
 
     [SerializeField]
     private AudioClip[] m_fishOnPlayerHurtAudio;
 
     [SerializeField]
+    [Range(0.0f, 1.0f)]
+    private float m_fishOnPlayerHurtVol = 1.0f;
+
+
+    [SerializeField]
     private AudioClip[] m_fishOnOarCollisionAudio;
+
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
+    private float m_fishOnOarCollisionVol = 1.0f;
+
 
     [SerializeField]
     private AudioClip m_oarEnterWaterAudio;
 
     [SerializeField]
+    [Range(0.0f, 1.0f)]
+    private float m_oarEnterWaterVol = 1.0f;
+
+    [SerializeField]
     private AudioClip m_oarExitWaterAudio;
+
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
+    private float m_oarExitWaterVol = 1.0f;
+
+
+    [SerializeField]
+    private AudioClip m_bgAmbience;
+
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
+    private float m_bgAmbienceVol = 1.0f;
+
+
+    [SerializeField]
+    private AudioClip m_bgMusic;
+
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
+    private float m_bgMusicVol = 1.0f;
+
 
 
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         _instance = this;
     }
 
     public void PlayBoatCollisionSFX() {
-        AudioSource.PlayClipAtPoint(m_boatCollisionAudio, Vector3.zero);
+        AudioSource.PlayClipAtPoint(m_boatCollisionAudio, Vector3.zero, m_boatCollisionVol);
     }
 
     public void PlayOarCollisionSFX() {
-        AudioSource.PlayClipAtPoint(m_oarCollisionAudio, Vector3.zero);
+        AudioSource.PlayClipAtPoint(m_oarCollisionAudio, Vector3.zero, m_oarCollisionVol);
     }
 
 
-    private void playRandomAudio(AudioClip[] audioClips) {
+    private void playRandomAudio(AudioClip[] audioClips, float vol) {
         int randInt = Random.Range(0,audioClips.Length);
-        AudioSource.PlayClipAtPoint(audioClips[randInt], Vector3.zero);
+        AudioSource.PlayClipAtPoint(audioClips[randInt], Vector3.zero, vol);
 
     }
 
-    private void hurtPlayer(AudioClip hitAudio, AudioClip[] hurtAudio) {
-        AudioSource.PlayClipAtPoint(hitAudio, Vector3.zero);
-        playRandomAudio(hurtAudio);
+    private void hurtPlayer(AudioClip hitAudio, AudioClip[] hurtAudio, float hitVol, float hurtVol) {
+        AudioSource.PlayClipAtPoint(hitAudio, Vector3.zero, hitVol);
+        playRandomAudio(hurtAudio, hurtVol);
     }
 
 
     public void PlayOarOnPlayerCollisionSFX() {
-        hurtPlayer(m_oarOnPlayerCollisionAudio, m_oarOnPlayerHurtAudio);
+        hurtPlayer(m_oarOnPlayerCollisionAudio, m_oarOnPlayerHurtAudio,
+            m_oarOnPlayerCollisionVol, m_oarOnPlayerHurtVol);
     }
 
     public void PlayFishOnPlayerCollisionSFX() {
-        hurtPlayer(m_fishOnPlayerCollisionAudio, m_fishOnPlayerHurtAudio);
+        hurtPlayer(m_fishOnPlayerCollisionAudio, m_fishOnPlayerHurtAudio,
+            m_fishOnOarCollisionVol, m_fishOnPlayerHurtVol);
     }
 
     public void PlayFishOnOarCollisionSFX() {
-        playRandomAudio(m_fishOnOarCollisionAudio);
+        playRandomAudio(m_fishOnOarCollisionAudio, m_fishOnOarCollisionVol);
     }
 
     public void PlayOarEnterWaterSFX() {
-        AudioSource.PlayClipAtPoint(m_oarEnterWaterAudio, Vector3.zero);
+        AudioSource.PlayClipAtPoint(m_oarEnterWaterAudio, Vector3.zero, m_oarEnterWaterVol);
     }
 
     public void PlayOarExitWaterSFX() {
-        AudioSource.PlayClipAtPoint(m_oarExitWaterAudio, Vector3.zero);
+        AudioSource.PlayClipAtPoint(m_oarExitWaterAudio, Vector3.zero, m_oarExitWaterVol);
     }
+
+    public AudioSource PlayLoopingAudio(AudioClip clip, float vol) {
+        AudioSource audioSource = new GameObject(clip.name).AddComponent<AudioSource>();
+        audioSource.loop = true;
+        audioSource.clip = clip;
+        audioSource.volume = vol;
+        audioSource.Play();
+        return audioSource;
+    }
+
+    public void PlayBGAmbience() {
+        PlayLoopingAudio(m_bgAmbience, m_bgAmbienceVol);
+    }
+
+    public AudioSource PlayBGM() {
+        return PlayLoopingAudio(m_bgMusic, m_bgMusicVol);
+    }
+
     
 }
